c4f8a412cfb778b6aedeeecc08ffe8407d49b0af
Nisrine Moumouh <nisrine.moumouh@infosupport.com>
2024-10-25 11:41:01 +0200

    adding device preview to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
