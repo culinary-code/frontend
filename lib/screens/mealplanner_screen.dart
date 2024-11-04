6746be9825db16b6104fb8ba37d839f1ec398e25
nisrine-m <nisrine.moumouh@infosupport.com>
2024-11-04 10:32:00 +0100

    2 als developer wil ik een navigatiebar toevoegen zodat ik tussen verschillende schermen kan navigeren (#41)
    * adding navbar + screens
    
    * adding details + cleaning code
    
    * commenting preview to not cause issues

---
+++ lib/screens/mealplanner_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MealplannerScreen extends StatelessWidget {
+  const MealplannerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Mealplanner Screen"),
+    );
+  }
+}
