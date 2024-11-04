20dd02c72efa749b9688c2b36c1048cd45f26083
nisrine-m <nisrine.moumouh@infosupport.com>
2024-11-04 16:18:00 +0100

    32 als developer wil ik een home pagina aanmaken zodat deze visueel beschikbaar is voor de gebruiker (#42)
    * homescreen draft afgewerkt
    
    * opmaak aanpassingen
    
    * fixing searchbar
    
    * moved hardcoded data to recipe class
    
    * experimenteren met fittedbox/overflow
    
    * commenting preview to prevent problems

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,165 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/models/recipe.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Home Screen"),
+    return MaterialApp(
+      home: const RecipeOverview(),
+      title: 'Culinary Code',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
+        // Theme of Project
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class RecipeOverview extends StatefulWidget {
+  const RecipeOverview({super.key});
+
+  @override
+  State<RecipeOverview> createState() => _RecipeOverviewState();
+}
+
+class _RecipeOverviewState extends State<RecipeOverview> {
+  final List<Recipe> _recipes = Recipe.recipeList();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Vind jouw recept!")),
+      body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Waar heb je trek in?',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 4),
+              const TextField(
+                decoration: InputDecoration(
+                    hintText: 'Aardappel',
+                    isDense: true,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8.0))),
+                    suffixIcon: Icon(Icons.search)),
+              ),
+              const SizedBox(
+                height: 16.0,
+              ),
+              Expanded(
+                  child: ListView.builder(
+                itemCount: _recipes.length,
+                // TODO: als je echte recepten ophaald limiteren naar bv 4
+                itemBuilder: (context, index) {
+                  return RecipeCard(
+                      recipeName: _recipes[index].recipeName,
+                      score: _recipes[index].score,
+                      isFavorited: _recipes[index].isFavorited,
+                      onFavoriteToggle: () {
+                        setState(() {
+                          _recipes[index].isFavorited =
+                              !_recipes[index].isFavorited;
+                        });
+                      });
+                },
+              ))
+            ],
+          )),
+    );
+  }
+}
+
+class RecipeCard extends StatelessWidget {
+  final String recipeName;
+  final double score;
+  final bool isFavorited;
+  final VoidCallback onFavoriteToggle;
+
+  const RecipeCard(
+      {super.key,
+      required this.recipeName,
+      required this.score,
+      required this.isFavorited,
+      required this.onFavoriteToggle});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 15.0),
+      child: Row(
+        children: [
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 4),
+            width: 100,
+            height: 100,
+            color: Colors.blueGrey,
+            child: const Icon(Icons.image, color: Colors.blueGrey),
+          ),
+          const SizedBox(width: 10),
+          Expanded(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              FittedBox(
+                child: Row(
+                  children: [
+                    Text(
+                      recipeName,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14
+                      ),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const SizedBox(width: 4),
+                    GestureDetector(
+                      onTap: onFavoriteToggle,
+                      child: Icon(
+                          isFavorited ? Icons.favorite : Icons.favorite_border,
+                          size: 18,
+                          color: isFavorited ? Colors.red : Colors.blueGrey),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 30),
+              Row(
+                children: [
+                  ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blueGrey[50]),
+                      child: const Text("Open")),
+                  const SizedBox(width: 8),
+                  Row(
+                    children: [
+                      Text(score.toStringAsFixed(1)),
+                      if (score > 0 && score < 5)
+                        const Icon(Icons.star_half, size: 16)
+                      else if (score == 0)
+                        const Icon(Icons.star_outline, size: 16)
+                      else if (score == 5)
+                        const Icon(Icons.star, size: 16)
+                    ],
+                  )
+                ],
+              )
+            ],
+          ))
+        ],
+      ),
     );
   }
 }
