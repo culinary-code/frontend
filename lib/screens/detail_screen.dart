008e43f5f7092a66faa9be2223a3ad1a8108e4b5
Nisrine Moumouh <nisrine.moumouh@infosupport.com>
2024-11-05 09:04:29 +0100

    adding header to detail page

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -6,15 +6,51 @@ class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Detail Screen"),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
+        appBar: AppBar(title: const Text("Laten we koken!")),
+        body: const RecipeHeader());
+  }
+}
+
+class RecipeHeader extends StatefulWidget {
+  const RecipeHeader({super.key});
+
+  @override
+  State<RecipeHeader> createState() => _RecipeHeaderState();
+}
+
+class _RecipeHeaderState extends State<RecipeHeader> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Center(
+            child: Text(
+              "Friet met stoofvlees",
+              style: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Image.asset('images/default_image.png')
+        ],
       ),
     );
   }
 }
+
+/*class MealOverview extends StatefulWidget {
+  const MealOverview({super.key});
+
+  @override
+  State<MealOverview> createState() => _MealOverviewState();
+}
+
+class _MealOverviewState extends State<MealOverview> {
+  //final List<Ingredient> _ingredients =
+
+
+}*/
