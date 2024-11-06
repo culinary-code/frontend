a6193acae2886ac4c199b520a76d2f0370e5caac
Nisrine Moumouh <nisrine.moumouh@infosupport.com>
2024-11-06 08:30:46 +0100

    adding griditems + cleaner design

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/screens/weekoverview_screen.dart';
-import 'package:input_quantity/input_quantity.dart';
 
-import '../models/ingredient.dart';
 import '../models/instruction.dart';
 
 class DetailScreen extends StatelessWidget {
@@ -50,15 +48,36 @@ class _DetailOverviewState extends State<DetailOverview> {
     return Scaffold(
         body: SingleChildScrollView(
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             RecipeHeader(
                 isFavorited: isFavorited, onFavoriteToggle: toggleFavorite),
             const SizedBox(height: 16),
-            const InformationOverview(),
-            const SizedBox(height: 16),
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.blueGrey[200],
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey.withOpacity(0.2),
+                        spreadRadius: 5,
+                        blurRadius: 8,
+                        offset: const Offset(0, 5))
+                  ]),
+              child: const RecipeDetailsGrid(),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                  'Stoofvlees is een gerecht van langzaam gegaard rundvlees in een rijke saus op basis van bier of bouillon. '
+                  'Het vlees wordt boterzacht en vol van smaak, ideaal comfortfood voor koude dagen.',
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(fontSize: 18)),
+            ),
+            const PortionSelector(),
+            const SizedBox(height: 16.0),
             IngredientsOverview(ingredientList: _ingredients),
             const SizedBox(height: 16.0),
             InstructionsOverview(instructionsSteps: _instructionSteps),
@@ -137,40 +156,140 @@ class _RecipeHeaderState extends State<RecipeHeader> {
   }
 }
 
-/*
-class IngredientsOverview extends StatelessWidget {
-  final List<String> ingredientList;
-
-  const IngredientsOverview({super.key, required this.ingredientList});
+class RecipeDetailsGrid extends StatelessWidget {
+  const RecipeDetailsGrid({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 50),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          const Text(
-            "Ingrediënten",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(height: 10),
-          //...ingredientList.map((ingredient) => Text(ingredient))
-          ...ingredientList.asMap().entries.map((entry) {
-            String ingredient = entry.value;
-            return Text(
-              "• $ingredient",
-              style: const TextStyle(fontSize: 16),
-            );
-          })
+          GridItem(icon: Icons.timer, label: '30 min'),
+          GridItem(icon: Icons.dinner_dining, label: 'Avondeten'),
+          GridItem(icon: Icons.thermostat, label: 'Gemiddeld')
         ],
       ),
     );
   }
-}*/
+}
+
+class GridItem extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  const GridItem({super.key, required this.icon, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(icon, color: Colors.blueGrey[800], size: 30),
+        const SizedBox(height: 4),
+        Text(
+          label,
+          style: const TextStyle(
+            fontSize: 14,
+            color: Colors.black,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class PortionSelector extends StatefulWidget {
+  const PortionSelector({super.key});
+
+  @override
+  State<PortionSelector> createState() => _PortionSelectorState();
+}
+
+class _PortionSelectorState extends State<PortionSelector> {
+  int portions = 2;
+
+  void addPortions() {
+    setState(() {
+      portions++;
+    });
+  }
+
+  void removePortions() {
+    setState(() {
+      if (portions > 1) portions--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Icon(
+                    Icons.local_dining,
+                    size: 30,
+                    color: Colors.blueGrey[800],
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Text(
+                    'Porties',
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blueGrey[800]),
+                  ),
+                  const SizedBox(
+                    width: 12,
+                  ),
+                  GestureDetector(
+                    onTap: removePortions,
+                    child: CircleAvatar(
+                      radius: 10,
+                      backgroundColor: Colors.blueGrey[200],
+                      child: Icon(
+                        Icons.remove,
+                        color: Colors.blueGrey[800],
+                        size: 18,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Text(
+                    '$portions',
+                    style: const TextStyle(fontSize: 22),
+                  ),
+                  const SizedBox(width: 10),
+                  GestureDetector(
+                    onTap: addPortions,
+                    child: CircleAvatar(
+                      radius: 10,
+                      backgroundColor: Colors.blueGrey[200],
+                      child: Icon(
+                        Icons.add,
+                        color: Colors.blueGrey[800],
+                        size: 18,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
 
 class IngredientsOverview extends StatelessWidget {
   final List<String> ingredientList;
@@ -184,14 +303,14 @@ class IngredientsOverview extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Table(
-            border: TableBorder.all(color: Colors.black),
+            border: TableBorder.all(color: Colors.white24),
             defaultVerticalAlignment: TableCellVerticalAlignment.middle,
             children: [
-              const TableRow(
+              TableRow(
                   decoration: BoxDecoration(
-                    color: Colors.redAccent,
+                    color: Colors.blueGrey[200],
                   ),
-                  children: [
+                  children: const [
                     TableCell(
                       verticalAlignment: TableCellVerticalAlignment.middle,
                       child: Padding(
@@ -288,48 +407,6 @@ class InstructionsOverview extends StatelessWidget {
               style: const TextStyle(fontSize: 18),
             );
           }),
-        ],
-      ),
-    );
-  }
-}
-
-class InformationOverview extends StatefulWidget {
-  const InformationOverview({super.key});
-
-  @override
-  State<InformationOverview> createState() => _InformationOverviewState();
-}
-
-class _InformationOverviewState extends State<InformationOverview> {
-  @override
-  Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(8),
-      child: Row(
-        children: [
-          Icon(Icons.person),
-          Text("Serves"),
-          Center(
-            child: Padding(
-              padding: EdgeInsets.all(20.0),
-              child: InputQty(
-                initVal: 0, // aanpassen naar ingesteld aantal personen
-                minVal: 0,
-                maxVal: 100,
-                steps: 1,
-                qtyFormProps: QtyFormProps(enableTyping: true),
-                decoration: QtyDecorationProps(
-                  isBordered: true,
-                  contentPadding: EdgeInsets.only(left: 15, right: 15, top: 5, bottom: 5),
-                  isDense: true,
-                  width: 10,
-                  minusBtn: Icon(Icons.remove, color: Colors.blue),
-                  plusBtn: Icon(Icons.add, color: Colors.blue),
-                ),
-              ),
-            ),
-          )
         ],
       ),
     );
