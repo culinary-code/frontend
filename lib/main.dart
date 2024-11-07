c6c7d5ffb1df1fb5f0d8c54aec3324dd34ec4338
HannesC <hannes.coenen@infosupport.com>
2024-11-07 13:20:31 +0100

    main load env file

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:frontend/navigation_menu.dart';
+import 'package:frontend/registrationScreen.dart';
 
-void main() {
+void main() async {
+   await dotenv.load(fileName: ".env");
    runApp(const MyApp());
     /*
     runApp(DevicePreview(
