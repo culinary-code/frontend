0306f18254b5c58ca410d371e42d141bb7292bcb
nisrine-m <nisrine.moumouh@infosupport.com>
2024-11-07 14:31:54 +0100

    33 als developer wil ik een shoppinglist pagina aanmaken zodat deze visueel beschikbaar is voor de gebruiker (#45)
    * grocerylist + delete and add
    
    * reeds bestaande boodschappen aanpassen als je deze opnieuw probeert toe te voegen + ongedaanmaak knop
    
    * kleine veranderingen
    
    * changes README
    
    * adding swipe icon
    
    * paar changes

---
+++ lib/models/groceryListItem.dart
@@ -0,0 +1,7 @@
+class GroceryListItem {
+  String productName;
+  int quantity;
+  String measurement;
+
+  GroceryListItem({required this.productName, required this.quantity, required this.measurement});
+}
