0157bff0dcfe89a638a5028e24ff81c6aa22cfce
HannesC <hannes.coenen@infosupport.com>
2024-11-20 16:12:39 +0100

    merge main and import recipecard

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,9 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:frontend/models/recipes/recipe.dart';
 import 'package:frontend/models/recipes/recipe_filter.dart';
 import 'package:frontend/screens/create_recipe_screen.dart';
-import 'package:frontend/screens/detail_screen.dart';
 import 'package:frontend/state/RecipeFilterOptionsProvider.dart';
-import 'package:http/http.dart' as http;
+import 'package:frontend/widgets/recipe_card.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -303,143 +302,6 @@ class _RecipeOverviewState extends State<RecipeOverview> {
   }
 }
 
-class RecipeCard extends StatelessWidget {
-  final String recipeId;
-  final String recipeName;
-  final double score;
-  final bool isFavorited;
-  final VoidCallback onFavoriteToggle;
-  final String imageUrl;
-
-  const RecipeCard(
-      {super.key,
-      required this.recipeId,
-      required this.recipeName,
-      required this.score,
-      required this.isFavorited,
-      required this.onFavoriteToggle,
-      required this.imageUrl});
-
-  Future<bool> _checkImageUrl(String url) async {
-    try {
-      final response = await http.get(Uri.parse(url));
-      return response.statusCode == 200;
-    } catch (e) {
-      print('Error checking image URL: $e');
-      return false;
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 15.0),
-      child: GestureDetector(
-        onTap: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => DetailScreen(
-                        recipeId: recipeId,
-                      )));
-        },
-        child: Row(
-          children: [
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 4),
-              width: 130,
-              height: 130,
-              color: Colors.blueGrey,
-              child: FutureBuilder<bool>(
-                future: _checkImageUrl(imageUrl),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else if (snapshot.hasError || !snapshot.data!) {
-                    return const Center(
-                      child: Icon(
-                        Icons.broken_image,
-                        size: 50,
-                        color: Colors.grey,
-                      ),
-                    );
-                  } else {
-                    return Image.network(
-                      imageUrl,
-                    );
-                  }
-                },
-              ),
-            ),
-            const SizedBox(width: 10),
-            Expanded(
-                child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        recipeName,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 20),
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                    const SizedBox(width: 4),
-                    GestureDetector(
-                      onTap: onFavoriteToggle,
-                      child: Icon(
-                          isFavorited ? Icons.favorite : Icons.favorite_border,
-                          size: 25,
-                          color: isFavorited ? Colors.red : Colors.blueGrey),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 30),
-                Row(
-                  children: [
-                    ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => DetailScreen(
-                                        recipeId: recipeId,
-                                      )));
-                        },
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.blueGrey[50]),
-                        child: const Text(
-                          "Open",
-                          style: TextStyle(fontSize: 18),
-                        )),
-                    const SizedBox(width: 8),
-                    Row(
-                      children: [
-                        Text(
-                          score.toStringAsFixed(1),
-                          style: const TextStyle(fontSize: 18),
-                        ),
-                        if (score > 0 && score < 5)
-                          const Icon(Icons.star_half, size: 22)
-                        else if (score == 0)
-                          const Icon(Icons.star_outline, size: 22)
-                        else if (score == 5)
-                          const Icon(Icons.star, size: 22)
-                      ],
-                    )
-                  ],
-                )
-              ],
-            ))
-          ],
-        ),
-      ),
-    );
-  }
-}
 
 class FilterPopup extends StatelessWidget {
   final FilterType initialFilter;
