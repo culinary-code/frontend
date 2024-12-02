2d49aa0e01b7578384180f8ac8bd0eef21e4f14a
nisrine-m <nisrine.moumouh@infosupport.com>
2024-12-02 13:02:18 +0100

    94 call toevoegen in frontend om favoriete recepten op te (#95)
    * get favorite recipes from backend
    
    * small change
    
    * deleting unused

--- lib/services/favorite_recipes_service.dart
+++ lib/services/favorite_recipes_service.dart
@@ -1,15 +1,25 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'dart:convert';
+
 import 'package:frontend/models/recipes/recipe.dart';
 
+import 'api_client.dart';
+
 class FavoriteRecipeService {
-  String get backendUrl =>
-      dotenv.env['BACKEND_BASE_URL'] ??
-          (throw Exception('Environment variable BACKEND_BASE_URL not found'));
+  Future<List<Recipe>> getFavoriteRecipes() async {
+    final endpoint = 'api/Account/getFavoriteRecipes';
+    final apiClient = await ApiClient.create();
+    final response = await apiClient.authorizedGet(endpoint);
 
-  Future<List<Recipe>> getDummyFavoriteRecipes() async {
-
-    var dummyRecipes = Recipe.recipeList();
-    List<Recipe> favoriteRecipes = dummyRecipes.where((recipe) => recipe.isFavorited).toList();
-    return favoriteRecipes;
+    if (response.statusCode == 200) {
+      final List<dynamic> dynamicRecipes = json.decode(response.body);
+      final List<Recipe> favoriteRecipes = dynamicRecipes.map((dynamic recipe) {
+        final Recipe mappedRecipe = Recipe.fromJson(recipe);
+        mappedRecipe.isFavorited = true;
+        return mappedRecipe;
+      }).toList();
+      return favoriteRecipes;
+    } else {
+      return [];
+    }
   }
 }
