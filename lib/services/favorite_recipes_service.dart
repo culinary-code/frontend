cc8ec2c2b557b2d73f52711b49868e4311426b7e
NisrineM <nisrine.moumouh@infosupport.com>
2024-12-02 11:55:35 +0100

    deleting unused

--- lib/services/favorite_recipes_service.dart
+++ lib/services/favorite_recipes_service.dart
@@ -1,22 +1,10 @@
 import 'dart:convert';
 
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:frontend/models/recipes/recipe.dart';
 
 import 'api_client.dart';
 
 class FavoriteRecipeService {
-  String get backendUrl =>
-      dotenv.env['BACKEND_BASE_URL'] ??
-      (throw Exception('Environment variable BACKEND_BASE_URL not found'));
-
-  Future<List<Recipe>> getDummyFavoriteRecipes() async {
-    var dummyRecipes = Recipe.recipeList();
-    List<Recipe> favoriteRecipes =
-        dummyRecipes.where((recipe) => recipe.isFavorited).toList();
-    return favoriteRecipes;
-  }
-
   Future<List<Recipe>> getFavoriteRecipes() async {
     final endpoint = 'api/Account/getFavoriteRecipes';
     final apiClient = await ApiClient.create();
